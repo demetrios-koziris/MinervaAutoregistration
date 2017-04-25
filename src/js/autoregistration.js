8dec55e1f024ec3badc36815c4f912b5bb0ee402
Demetrios Koziris <dwkoziris@gmail.com>
2017-04-24 21:36:32 -0400

    add license info

--- src/js/autoregistration.js
+++ src/js/autoregistration.js
@@ -1,3 +1,17 @@
+/*
+Minerva Autoregistration is a Chrome Extension that makes autoregistration possible in Minerva
+Copyright (C) 2017 Demetrios Koziris
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License 
+as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+A copy of the GNU General Public License is provided in the LICENSE.txt file along with this program.  
+The GNU General Public License can also be found at <http://www.gnu.org/licenses/>.
+*/
+
 //jshint esversion: 6
 
 
